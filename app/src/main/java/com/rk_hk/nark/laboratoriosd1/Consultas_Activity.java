883e83efc1296cc1b4ac6424ff1ba4e66bf910fb
LeoMacs <leomacs1009@gmail.com>
2017-05-05 20:23:39 -0500

    modificadoLayoutPrueba

---
+++ app/src/main/java/com/rk_hk/nark/laboratoriosd1/Consultas_Activity.java
@@ -0,0 +1,13 @@
+package com.rk_hk.nark.laboratoriosd1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Consultas_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.consultas_vistas);
+    }
+}
