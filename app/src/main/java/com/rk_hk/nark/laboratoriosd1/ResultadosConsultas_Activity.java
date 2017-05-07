e92bc940350fee789dc78ba60cbe62ca50c4e1e5
LeoMacs <leomacs1009@gmail.com>
2017-05-07 13:30:49 -0500

    Modifique una mrd esperemos que suba

---
+++ app/src/main/java/com/rk_hk/nark/laboratoriosd1/ResultadosConsultas_Activity.java
@@ -0,0 +1,13 @@
+package com.rk_hk.nark.laboratoriosd1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResultadosConsultas_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resultadosconsultas_vista);
+    }
+}
