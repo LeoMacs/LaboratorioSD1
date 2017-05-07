74b217f83bb97f36f5d4208258144a8f41d4e161
LeoMacs <leomacs1009@gmail.com>
2017-05-07 12:30:07 -0500

    modificadoLayoutPrueba

---
+++ app/src/main/java/com/rk_hk/nark/laboratoriosd1/ResultadosConsultas_Activity.java
@@ -0,0 +1,13 @@
+package com.rk_hk.nark.laboratoriosd1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultadosConsultas_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resultadosconsultas_vista);
+    }
+}
