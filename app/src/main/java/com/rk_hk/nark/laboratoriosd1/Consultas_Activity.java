fc421a2666f3a508ad2f166c11146a12c474bb27
LeoMacs <leomacs1009@gmail.com>
2017-05-07 16:32:40 -0500

    modificadoLayoutPrueba

--- app/src/main/java/com/rk_hk/nark/laboratoriosd1/Consultas_Activity.java
+++ app/src/main/java/com/rk_hk/nark/laboratoriosd1/Consultas_Activity.java
@@ -1,11 +1,13 @@
 package com.rk_hk.nark.laboratoriosd1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
-public class Consultas_Activity extends AppCompatActivity {
+public class Consultas_Activity extends AppCompatActivity implements View.OnClickListener{
     ListView lvdatos;
     Button btConsultar;
 
@@ -14,5 +16,18 @@ public class Consultas_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.consultas_vistas);
         btConsultar=(Button)findViewById(R.id.btConsultar);
+        btConsultar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.btConsultar){
+            Intent intent=new Intent(Consultas_Activity.this,ResultadosConsultas_Activity.class);
+            startActivity(intent);
+
+
+
+        }
+
     }
 }
